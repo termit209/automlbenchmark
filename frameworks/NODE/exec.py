891ced9c3126c4e12ebe8d2ee8c40df6faa5fb1d
Alexey Korolev <ya.korol209@yandex.ru>
2021-02-04 13:13:06 +0700

    correct something

--- frameworks/NODE/exec.py
+++ frameworks/NODE/exec.py
@@ -11,6 +11,7 @@ from . import lib
 import math
 import time
 import numpy as np
+from sklearn.metrics import classification_report
 
 from amlb.benchmark import TaskConfig
 from amlb.data import Dataset
@@ -26,21 +27,20 @@ def run(dataset, config):
     log.info(f"\n**** NODE")
 
     is_classification = config.type == 'classification'
+    
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
+    np.random.seed(config.seed)
 
     X_train, X_test = dataset.train.X_enc, dataset.test.X_enc
     y_train, y_test = dataset.train.y_enc, dataset.test.y_enc
-    X_test = X_test.astype(np.float32)
-    X_train = X_train.astype(np.float32)
-    y_train = y_train.astype(np.long)
-    y_test = y_test.astype(np.long)
-
+    print(X_train.dtype, y_train.dtype)
     training_params = {k: v for k, v in config.framework_params.items() if not k.startswith('_')}
-    n_jobs = config.framework_params.get('_n_jobs', config.cores)  # useful to disable multicore, regardless of the dataset config
 
-    log.info("Running RandomForest with a maximum time of {}s on {} cores.".format(config.max_runtime_seconds, n_jobs))
-    log.warning("We completely ignore the requirement to stay within the time limit.")
-    log.warning("We completely ignore the advice to optimize towards metric: {}.".format(config.metric))
-
+    #log.info("Running RandomForest with a maximum time of {}s on {} cores.".format(config.max_runtime_seconds, n_jobs))
+    #log.warning("We completely ignore the requirement to stay within the time limit.")
+    #log.warning("We completely ignore the advice to optimize towards metric: {}.".format(config.metric))
+    print(config)
 
 
 
@@ -48,19 +48,23 @@ def run(dataset, config):
     print(f'Using {device}')
     X_train, X_val, y_train, y_val = train_test_split( X_train, y_train, test_size=0.2, random_state=42)
     num_features = X_train.shape[1]
-    
+    ts = math.floor(time.time())
+    experiment_name = f'node_adult_{ts}'
     if is_classification:
+        X_test = X_test.astype(np.float32)
+        X_val = X_val.astype(np.float32)
+        X_train = X_train.astype(np.float32)
+        y_train = y_train.astype(np.long)
+        y_test = y_test.astype(np.long)
+        y_val = y_val.astype(np.long)
         num_classes = len(set(y_train))
-        ts = math.floor(time.time())
-        experiment_name = f'node_adult_{ts}'
+
         model = nn.Sequential(lib.DenseBlock(num_features,layer_dim=128,num_layers=3,
                                          tree_dim=num_classes,flatten_output=False,depth=6,
                                          choice_function=lib.entmax15,bin_function=lib.entmoid15),
                           lib.Lambda(lambda x: x[..., :num_classes].mean(dim=-2)),).to(device)
 
-        with torch.no_grad():
-            res = model(torch.as_tensor(X_train[:2000], device=device))
-        # trigger data-aware init
+
         trainer = lib.Trainer(model=model, loss_function=F.cross_entropy,experiment_name=experiment_name,
                           warm_start=False,Optimizer=QHAdam,optimizer_params=dict(nus=(0.7, 1.0),
                                                                                   betas=(0.95, 0.998)),
@@ -71,51 +75,48 @@ def run(dataset, config):
         early_stopping_rounds = 2500
         report_frequency = 1000
 
-    for batch in lib.iterate_minibatches(X_train,
+        for batch in lib.iterate_minibatches(X_train,
                                      y_train,
                                      batch_size=512, 
                                      shuffle=True,
                                      epochs=float('inf')):
-        metrics = trainer.train_on_batch(*batch, device=device)
+            metrics = trainer.train_on_batch(*batch, device=device)
     
-        loss_history.append(metrics['loss'])
+            loss_history.append(metrics['loss'])
 
-        if trainer.step % report_frequency == 0:
-            trainer.save_checkpoint()
-            trainer.average_checkpoints(out_tag='avg')
-            trainer.load_checkpoint(tag='avg')
-            err = trainer.evaluate_classification_error(
-            X_val,
-            y_val,
-            device=device,
-            batch_size=128)
+            if trainer.step % report_frequency == 0:
+                trainer.save_checkpoint()
+                trainer.average_checkpoints(out_tag='avg')
+                trainer.load_checkpoint(tag='avg')
+                err = trainer.evaluate_classification_error(X_val,y_val,device=device,batch_size=128)
         
-            if err < best_val_err:
-                best_val_err = err
-                best_step = trainer.step
-                trainer.save_checkpoint(tag='best')
+                if err < best_val_err:
+                    best_val_err = err
+                    best_step = trainer.step
+                    trainer.save_checkpoint(tag='best')
         
-            err_history.append(err)
-            trainer.load_checkpoint()  # last
-            trainer.remove_old_temp_checkpoints()
+                err_history.append(err)
+                trainer.load_checkpoint()  # last
+                trainer.remove_old_temp_checkpoints()
 
-            print("Loss %.5f" % (metrics['loss']))
-            print("Val Error Rate: %0.5f" % (err))
+                print("Loss %.5f" % (metrics['loss']))
+                print("Val Error Rate: %0.5f" % (err))
         
-        if trainer.step > best_step + early_stopping_rounds:
-            print('BREAK. There is no improvement for {} steps'.format(early_stopping_rounds))
-            print("Best step: ", best_step)
-            print("Best Val Error Rate: %0.5f" % (best_val_err))
-            break
+            if trainer.step > best_step + early_stopping_rounds:
+                print('BREAK. There is no improvement for {} steps'.format(early_stopping_rounds))
+                print("Best step: ", best_step)
+                print("Best Val Error Rate: %0.5f" % (best_val_err))
+                break
     else:
-        model = nn.Sequential(
-            lib.DenseBlock(in_features, 2048, num_layers=1, tree_dim=3, depth=6, flatten_output=False,
-                           choice_function=lib.entmax15, bin_function=lib.entmoid15),
-                           lib.Lambda(lambda x: x[..., 0].mean(dim=-1)),).to(device)
+        X_test = X_test.astype(np.float32)
+        X_val = X_val.astype(np.float32)
+        X_train = X_train.astype(np.float32)
+        y_train = y_train.astype(np.float32)
+        y_test = y_test.astype(np.float32)
+        y_val = y_val.astype(np.float32)
+        model = nn.Sequential(lib.DenseBlock(num_features, 2048, num_layers=1, tree_dim=3, depth=6, flatten_output=False,
+        choice_function=lib.entmax15, bin_function=lib.entmoid15), lib.Lambda(lambda x: x[..., 0].mean(dim=-1)),).to(device)
 
-
-        if torch.cuda.device_count() > 1:
-            model = nn.DataParallel(model)
         optimizer_params = { 'nus':(0.7, 1.0), 'betas':(0.95, 0.998) }
 
         trainer = lib.Trainer(model=model, loss_function=F.mse_loss,experiment_name=experiment_name,
@@ -130,7 +131,7 @@ def run(dataset, config):
         report_frequency = 100
 
 
-        for batch in lib.iterate_minibatches(data.X_train, data.y_train, batch_size=512, 
+        for batch in lib.iterate_minibatches(X_train, y_train, batch_size=512, 
                                              shuffle=True, epochs=float('inf')):
             metrics = trainer.train_on_batch(*batch, device=device)
     
@@ -140,7 +141,7 @@ def run(dataset, config):
                 trainer.save_checkpoint()
                 trainer.average_checkpoints(out_tag='avg')
                 trainer.load_checkpoint(tag='avg')
-                mse = trainer.evaluate_mse(data.X_valid, data.y_valid, device=device, batch_size=1024)
+                mse = trainer.evaluate_mse(X_valid, y_valid, device=device, batch_size=1024)
 
                 if mse < best_mse:
                     best_mse = mse
@@ -164,26 +165,28 @@ def run(dataset, config):
     with Timer() as predict:
         X_test = torch.as_tensor(X_test, device=device)
         with torch.no_grad():
-            predictions = lib.process_in_chunks(trainer.model, X_test, batch_size=512)
+            output = lib.process_in_chunks(trainer.model, X_test, batch_size=512)
         
         if is_classification:
-            probabilities = F.softmax(predictions, dim=1)
+            probabilities = F.softmax(output, dim=1)
             probabilities = probabilities.cpu().numpy()
+            predictions = np.argmax(probabilities, axis=1)
+            print(classification_report(y_test, predictions))
         else:
-            None
-        predictions = np.argmax(probabilities, axis=1)
-
+            predictions = output.cpu().numpy()
+        
+   
 
     save_predictions(dataset=dataset,
                      output_file=config.output_predictions_file,
                      probabilities=probabilities,
                      predictions=predictions,
-                     truth=y_test)
+                     truth=y_test,
+                     target_is_encoded=True)
                      
     return result(output_file=config.output_predictions_file,
                   predictions=predictions,
                   truth=y_test,
                   probabilities=probabilities,
-                  target_is_encoded=is_classification,
-                  training_duration=training.duration,
+                  target_is_encoded=True,
                   predict_duration=predict.duration)
